3e7cdb912612a789c1043e2385104f399a08bbdb
Dat Do <dodat@secure.janeto.com>
2019-02-25 15:46:58 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MenuBarComponent } from './layout/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { MenuBar } from './menu-bar/menu-bar.component';
+import { LayoutComponent } from './layout.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [],
+  providers: [],
+})
+export class LayoutModule { }
